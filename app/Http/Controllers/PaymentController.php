344da28f10c388c9f23b6aabc6c5f82435d9b1f0
tony <attila.kovacs92@gmail.com>
2022-05-09 11:21:52 +0200

    ipn 10

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -181,7 +181,7 @@ class PaymentController extends Controller
 		try {
 			$json['receiveDate'] = now(); 
 			// $signature = \codeBase64(hmacWithSha384('OMS52064302', $json));
-			$signature = base64_encode(hash_hmac('sha384', json_encode($json), 'secret'));
+			$signature = base64_encode(hash_hmac('sha256', 'OMS52064302' . json_encode($json), 'secret'));
 
 			return response($json, 200)
 				->header('signature', $signature);
