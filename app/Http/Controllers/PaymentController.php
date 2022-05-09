8dc84f0e8750edbbdbd17280a7d0387b2677968b
tony <attila.kovacs92@gmail.com>
2022-05-09 14:13:31 +0200

    ipn 12

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -19,214 +19,210 @@ use Throwable;
 
 class PaymentController extends Controller
 {
-	use MailTrait;
+    use MailTrait;
 
-	protected $config;
+    protected $config;
 
-	public function __construct()
-	{
-		$this->config = config('site.simplepay');
-	}
-
-
-	public function start(Request $request)
-	{	
-		$data = $request->all();
-		$applicant = Applicant::find($data['applicant']);
-		$appointment = Appointment::find($data['appointment']);
-
-		$trx = new SimplePayStart();
-
-		$currency = 'HUF';
-		$trx->addData('currency', $currency);
-
-		$trx->addConfig($this->config);
-		$trx->addData('total', 5000);
+    public function __construct()
+    {
+        $this->config = config('site.simplepay');
+    }
 
 
-		$trx->addItems(
-			array(
-				'ref' => 'CSE-' . $appointment->id,
-				'title' => 'Vizsgálat: ' . $appointment->medicalExamination->name,
-				'desc' => 'Vizsgálat előlege',
-				'amount' => 1,
-				'price' => 5000,
-				// 'tax' => '27',
-			)
-		);
+    public function start(Request $request)
+    {
+        $data = $request->all();
+        $applicant = Applicant::find($data['applicant']);
+        $appointment = Appointment::find($data['appointment']);
 
-		$trx->addData('maySelectEmail', true);
-		$trx->addData('maySelectInvoice', true);
+        $trx = new SimplePayStart();
 
-		$trx->addData('orderRef', str_replace(array('.', ':', '/'), "", @$_SERVER['SERVER_ADDR']) . @date("U", time()) . rand(1000, 9999));
+        $currency = 'HUF';
+        $trx->addData('currency', $currency);
+
+        $trx->addConfig($this->config);
+        $trx->addData('total', 5000);
 
 
-		$trx->addData('customer', $applicant->name);
+        $trx->addItems(
+            array(
+                'ref' => 'CSE-' . $appointment->id,
+                'title' => 'Vizsgálat: ' . $appointment->medicalExamination->name,
+                'desc' => 'Vizsgálat előlege',
+                'amount' => 1,
+                'price' => 5000,
+                // 'tax' => '27',
+            )
+        );
 
-		$trx->addData('threeDSReqAuthMethod', '02');
+        $trx->addData('maySelectEmail', true);
+        $trx->addData('maySelectInvoice', true);
 
-
-		$trx->addData('customerEmail', $applicant->email);
-
-
-		$trx->addData('language', 'HU');
-
-
-		if (isset($_REQUEST['twoStep'])) {
-			$trx->addData('twoStep', true);
-		}
-
-		$timeoutInSec = 600;
-		$timeout = @date("c", time() + $timeoutInSec);
-		$trx->addData('timeout', $timeout);
+        $trx->addData('orderRef', str_replace(array('.', ':', '/'), "", @$_SERVER['SERVER_ADDR']) . @date("U", time()) . rand(1000, 9999));
 
 
-		$trx->addData('methods', array('CARD'));
+        $trx->addData('customer', $applicant->name);
+
+        $trx->addData('threeDSReqAuthMethod', '02');
 
 
-		$trx->addData('url', $this->config['URL']);
-
-		// $trx->addGroupData('urls', 'success', $config['URLS_SUCCESS']);
-		// $trx->addGroupData('urls', 'fail', $config['URLS_FAIL']);
-		// $trx->addGroupData('urls', 'cancel', $config['URLS_CANCEL']);
-		// $trx->addGroupData('urls', 'timeout', $config['URLS_TIMEOUT']);
+        $trx->addData('customerEmail', $applicant->email);
 
 
-		$trx->addGroupData('mobilApp', 'simpleAppBackUrl', 'myAppS01234://payment/123456789');
+        $trx->addData('language', 'HU');
 
 
-		$trx->addGroupData('invoice', 'name', $applicant->name);
-		// $trx->addGroupData('invoice', 'company', '');
-		$trx->addGroupData('invoice', 'country', 'hu');
-		// $trx->addGroupData('invoice', 'state', 'Budapest');
-		$trx->addGroupData('invoice', 'city', $applicant->city);
-		$trx->addGroupData('invoice', 'zip', $applicant->zip);
-		$trx->addGroupData('invoice', 'address', $applicant->street);
-		// $trx->addGroupData('invoice', 'address2', 'Address 2');
-		$trx->addGroupData('invoice', 'phone', $applicant->phone);
+        if (isset($_REQUEST['twoStep'])) {
+            $trx->addData('twoStep', true);
+        }
 
-		$trx->formDetails['element'] = 'button';
+        $timeoutInSec = 600;
+        $timeout = @date("c", time() + $timeoutInSec);
+        $trx->addData('timeout', $timeout);
 
-		$trx->runStart();
 
-		$trx->getHtmlForm();
+        $trx->addData('methods', array('CARD'));
 
-		$appointment->payment()->create([
-			'status' => Status::START_PAYMENT,
-			'transaction_id' => $trx->returnData['transactionId'],
-			'order_ref' => $trx->returnData['orderRef']
-		]);
-		
+
+        $trx->addData('url', $this->config['URL']);
+
+        // $trx->addGroupData('urls', 'success', $config['URLS_SUCCESS']);
+        // $trx->addGroupData('urls', 'fail', $config['URLS_FAIL']);
+        // $trx->addGroupData('urls', 'cancel', $config['URLS_CANCEL']);
+        // $trx->addGroupData('urls', 'timeout', $config['URLS_TIMEOUT']);
+
+
+        $trx->addGroupData('mobilApp', 'simpleAppBackUrl', 'myAppS01234://payment/123456789');
+
+
+        $trx->addGroupData('invoice', 'name', $applicant->name);
+        // $trx->addGroupData('invoice', 'company', '');
+        $trx->addGroupData('invoice', 'country', 'hu');
+        // $trx->addGroupData('invoice', 'state', 'Budapest');
+        $trx->addGroupData('invoice', 'city', $applicant->city);
+        $trx->addGroupData('invoice', 'zip', $applicant->zip);
+        $trx->addGroupData('invoice', 'address', $applicant->street);
+        // $trx->addGroupData('invoice', 'address2', 'Address 2');
+        $trx->addGroupData('invoice', 'phone', $applicant->phone);
+
+        $trx->formDetails['element'] = 'button';
+
+        $trx->runStart();
+
+        $trx->getHtmlForm();
+
+        $appointment->payment()->create([
+            'status' => Status::START_PAYMENT,
+            'transaction_id' => $trx->returnData['transactionId'],
+            'order_ref' => $trx->returnData['orderRef']
+        ]);
+        
         return redirect()->to($trx->returnData['paymentUrl']);
-	}
+    }
 
-	public function back()
-	{
-		$trx = new SimplePayBack();
+    public function back()
+    {
+        $trx = new SimplePayBack();
 
-		$trx->addConfig($this->config);
+        $trx->addConfig($this->config);
 
 
-		//result
-		//-----------------------------------------------------------------------------------------
-		$result = array();
-		if (isset($_REQUEST['r']) && isset($_REQUEST['s'])) {
-			if ($trx->isBackSignatureCheck($_REQUEST['r'], $_REQUEST['s'])) {
-				$result = $trx->getRawNotification();
-			}
-		}
+        //result
+        //-----------------------------------------------------------------------------------------
+        $result = array();
+        if (isset($_REQUEST['r']) && isset($_REQUEST['s'])) {
+            if ($trx->isBackSignatureCheck($_REQUEST['r'], $_REQUEST['s'])) {
+                $result = $trx->getRawNotification();
+            }
+        }
 
-		if ($result['e'] === 'SUCCESS') {
+        if ($result['e'] === 'SUCCESS') {
+            $payment = Payment::query()
+                ->where('transaction_id', $result['t'])
+                ->where('order_ref', $result['o'])
+                ->firstOrFail();
+            
+            $payment->update(['status' => Status::END_PAYMENT]);
+            
+            $appointment = $payment->paymentable;
 
-			$payment = Payment::query()
-				->where('transaction_id', $result['t'])
-				->where('order_ref', $result['o'])
-				->firstOrFail();
-			
-			$payment->update(['status' => Status::END_PAYMENT]);
-			
-			$appointment = $payment->paymentable;
+            return redirect()->route('payments.greeting', $appointment->id);
+        } else {
+            $payment = Payment::query()
+                ->where('transaction_id', $result['t'])
+                ->where('order_ref', $result['o'])
+                ->firstOrFail();
 
-			return redirect()->route('payments.greeting', $appointment->id);
+            switch ($result['e']) {
+                case 'CANCEL':
+                    $payment->update(['status' => Status::CANCEL_PAYMENT]);
+                    break;
+                
+                case 'FAIL':
+                    $payment->update(['status' => Status::FAIL_PAYMENT]);
+                    break;
 
-		} else {
-			$payment = Payment::query()
-				->where('transaction_id', $result['t'])
-				->where('order_ref', $result['o'])
-				->firstOrFail();
+                case 'TIMEOUT':
+                    $payment->update(['status' => Status::TIMEOUT_PAYMENT]);
+                    break;
+            }
 
-			switch ($result['e']) {
-				case 'CANCEL':
-					$payment->update(['status' => Status::CANCEL_PAYMENT]);
-					break;
-				
-				case 'FAIL':
-					$payment->update(['status' => Status::FAIL_PAYMENT]);
-					break;
+            return redirect()->route('pages.payment-error', $payment->id);
+        }
+    }
 
-				case 'TIMEOUT':
-					$payment->update(['status' => Status::TIMEOUT_PAYMENT]);
-					break;
-			}
+    public function ipn(Request $request)
+    {
+        $json = $request->all();
 
-			return redirect()->route('pages.payment-error', $payment->id);
-		}
-	}
+        try {
+            $json['receiveDate'] = now();
+            // $signature = \codeBase64(hmacWithSha384('OMS52064302', $json));
+            $signature = base64_encode(hash_hmac('sha384', 'OMS52064302' . json_encode($json), 'secret'));
 
-	public function ipn(Request $request)
-	{
+            return response($json, 200)
+                ->header('Signature', $signature);
+            // ->header('signature', $request->header('signature'));
+            
+            $trx = new SimplePayIpn;
 
-		$json = $request->all();
+            $trx->addConfig($this->config);
+            $confirm = $trx->getIpnConfirmContent();
+            return $confirm;
 
-		try {
-			$json['receiveDate'] = now(); 
-			// $signature = \codeBase64(hmacWithSha384('OMS52064302', $json));
-			$signature = base64_encode(hash_hmac('sha384', 'OMS52064302' . json_encode($json), 'secret'));
+            // //check signature and confirm IPN
+            // //-----------------------------------------------------------------------------------------
+            if ($trx->isIpnSignatureCheck($json)) {
+                dd($json);
+                /**
+                 * Generates all response
+                 * Puts signature into header
+                 * Print response body
+                 *
+                 * Use this OR getIpnConfirmContent
+                 */
+                // return $trx->runIpnConfirm();
 
-			return response($json, 200)
-				->header('signature', $signature);
-				// ->header('signature', $request->header('signature'));
-			
-			$trx = new SimplePayIpn;
+                /**
+                 * Generates all response
+                 * Gets signature and response body
+                 *
+                 * You must set signeature in header and you must print response body!
+                 *
+                 * Use this OR runIpnConfirm()
+                 */
+                // $confirm = $trx->getIpnConfirmContent();
+            }
+        } catch (Throwable $e) {
+            Log::error($e);
+            dd($e);
+        }
 
-			$trx->addConfig($this->config);
-			$confirm = $trx->getIpnConfirmContent();
-			return $confirm;
+        //no need to print further information
+        exit;
+    }
 
-			// //check signature and confirm IPN
-			// //-----------------------------------------------------------------------------------------
-			if ($trx->isIpnSignatureCheck($json)) {
-				dd($json);
-				/**
-				 * Generates all response
-				 * Puts signature into header
-				 * Print response body
-				 *
-				 * Use this OR getIpnConfirmContent
-				 */
-				// return $trx->runIpnConfirm();
-
-				/**
-				 * Generates all response
-				 * Gets signature and response body
-				 *
-				 * You must set signeature in header and you must print response body!
-				 *
-				 * Use this OR runIpnConfirm()
-				 */
-				// $confirm = $trx->getIpnConfirmContent();
-			}
-
-		} catch (Throwable $e) {
-			Log::error($e);
-			dd($e);
-		}
-
-		//no need to print further information
-		exit;
-	}
-
-	public function paymentError(Payment $payment)
+    public function paymentError(Payment $payment)
     {
         return view('payments.error', [ 'transaction' => $payment]);
     }
