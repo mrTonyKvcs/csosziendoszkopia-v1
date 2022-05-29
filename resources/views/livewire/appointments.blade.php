0d5da991fd9301a11651a329d7b6f8dd2f98b3d5
tony <attila.kovacs92@gmail.com>
2022-05-29 18:38:55 +0200

    add new info message

--- resources/views/livewire/appointments.blade.php
+++ resources/views/livewire/appointments.blade.php
@@ -32,7 +32,8 @@
 						</svg>
 					</div>
 					<div class="flex-1 ml-3 md:flex md:justify-between">
-						<p class="text-blue-700 text-md">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+						<p class="text-blue-700 text-md"><strong>Kedves Betegünk!</strong> <br>Amennyiben  oltási igazolvánnyal nem rendelkezik, a gyomor- és vastagbéltükrözés elvégzéséhez negatív PCR teszt bemutatása szükséges, mely a vizsgálat időpontjához viszonyítva 48 óránál nem lehet korábbi. 
+						</p>
 					</div>
 				</div>
 			</div>
