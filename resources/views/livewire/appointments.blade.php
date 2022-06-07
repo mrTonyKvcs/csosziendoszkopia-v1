4bf5d758b40247a93a96f4770270fb8207fd55fd
tony <attila.kovacs92@gmail.com>
2022-06-07 10:17:27 +0200

    change button text

--- resources/views/livewire/appointments.blade.php
+++ resources/views/livewire/appointments.blade.php
@@ -121,7 +121,7 @@
 											type="submit"
 											class="relative inline-flex items-center px-2 py-2 -ml-px text-lg font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
 											>
-											Új időpont felvétele
+											Foglalás és fizetés
 											<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 												<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
 											</svg>
